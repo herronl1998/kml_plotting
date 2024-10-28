9289630250c4e98eb46febe4f5c64409d46274c0
herronl1998 <73509068+herronl1998@users.noreply.github.com>
2024-10-28 18:17:08 -0500

    123

--- plot_data.py
+++ plot_data.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 pickle_file_path = './data/pickle/' #path to the data
 save_file_path = './figures/' #path to where figures will be output
 alt_thresh = 3200.5 #threshold for above vs below altitude (in meters); 10000ft = 3048m; using 3200 for safety
-marker_size = 2
+marker_size = .5
 
 ###load the data
 all_data = pandas.DataFrame()
@@ -15,6 +15,9 @@ for filename in os.listdir(pickle_file_path):
     abs_path = os.path.abspath(pickle_file_path + filename)
     data = pandas.read_pickle(abs_path)
     all_data = pandas.concat([all_data, data])
+#parse the data so only USAF flights are included
+all_data = all_data.loc[all_data['mission_id'].str.startswith('AF')]
+
 
 def m_to_f(m):
     f = 3.28084 * m
@@ -32,7 +35,7 @@ ax.add_feature(cfeature.LAND)
 ax.gridlines(draw_labels = True)
 ax.scatter(all_data['lon'], all_data['lat'], c = 'blue', s = marker_size, label = f'All Tracks')
 ax.legend()
-plt.title('Full Tracks')
+plt.title('Tracks (2020-2023)')
 plt.savefig(os.path.abspath(save_file_path + 'all_tracks.png'), bbox_inches = 'tight')
 
 #both
@@ -44,7 +47,7 @@ ax.gridlines(draw_labels = True)
 ax.scatter(above_10k['lon'], above_10k['lat'], c = 'red', s = marker_size, label = f'> {round(m_to_f(alt_thresh))}ft')
 ax.scatter(below_10k['lon'], below_10k['lat'], c = 'green', s = marker_size, label = f'<= {round(m_to_f(alt_thresh))}ft')
 ax.legend()
-plt.title(f'Above vs Below {round(m_to_f(alt_thresh))}ft')
+plt.title(f'Tracks Above vs Below {round(m_to_f(alt_thresh))}ft (2020-2023)')
 plt.savefig(os.path.abspath(save_file_path + 'both.png'), bbox_inches = 'tight')
 
 #above
@@ -55,7 +58,7 @@ ax.add_feature(cfeature.LAND)
 ax.gridlines(draw_labels = True)
 ax.scatter(above_10k['lon'], above_10k['lat'], c = 'red', s = marker_size, label = f'> {round(m_to_f(alt_thresh))}ft')
 ax.legend()
-plt.title(f'Above {round(m_to_f(alt_thresh))}ft')
+plt.title(f'Tracks Above {round(m_to_f(alt_thresh))}ft (2020-2023)')
 plt.savefig(os.path.abspath(save_file_path + 'above.png'), bbox_inches = 'tight')
 
 #below
@@ -66,5 +69,7 @@ ax.add_feature(cfeature.LAND)
 ax.gridlines(draw_labels = True)
 ax.scatter(below_10k['lon'], below_10k['lat'], c = 'green', s = marker_size, label = f'<= {round(m_to_f(alt_thresh))}ft')
 ax.legend()
-plt.title(f'Below {round(m_to_f(alt_thresh))}ft')
+plt.title(f'Tracks Below {round(m_to_f(alt_thresh))}ft (2020-2023)')
 plt.savefig(os.path.abspath(save_file_path + 'below.png'), bbox_inches = 'tight')
+
+
